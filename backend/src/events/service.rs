2f929823ff9dddaf55d8ea0acae4af0f42c8465c
rainy-me <github@rainy.me>
2021-01-27 20:59:02 +0900

    feat: impl allow_query_by_slug

--- backend/src/events/service.rs
+++ backend/src/events/service.rs
@@ -1,25 +1,42 @@
 use super::model::*;
-use crate::sql::schema::events::{self, dsl::*};
 use crate::Context;
+use crate::{
+    sql::schema::events::{self, dsl::*},
+    utils::OrMessageError,
+};
 use diesel::prelude::*;
 use validator::Validate;
 
-pub fn get(ctx: &Context, event_id: i32) -> anyhow::Result<Event> {
+pub fn get(ctx: &Context, query: EventQueryPublic) -> anyhow::Result<Event> {
+    if let Some(event_slug) = query.slug {
+        let conn = ctx.pool.get()?;
+        return Ok(events.filter(slug.eq(event_slug)).first(&conn)?);
+    }
     let conn = ctx.pool.get()?;
-    Ok(events.find(event_id).first(&conn)?)
+    Ok(events.find(query.id.or_error("not found")?).first(&conn)?)
 }
 
 /// see https://github.com/diesel-rs/diesel/issues/2037
-pub fn get_public(ctx: &Context, event_id: i32) -> anyhow::Result<EventPublic> {
+pub fn get_public(ctx: &Context, query_input: EventQueryPublic) -> anyhow::Result<EventPublic> {
     let conn = ctx.pool.get()?;
+    let updates = page_view.eq(page_view + 1);
+    let returns = (
+        id, slug, title, body, genre, tag, fee, ogp_img, start_at, end_at, publish_at, updated_at,
+        page_view,
+    );
+    if let Some(_slug) = query_input.slug {
+        return Ok(diesel::update(events.filter(slug.eq(_slug)))
+            .set(updates)
+            .returning(returns)
+            .get_result::<EventPublic>(&conn)?);
+    }
 
-    Ok(diesel::update(events.filter(id.eq(event_id)))
-        .set(page_view.eq(page_view + 1))
-        .returning((
-            id, slug, title, body, genre, tag, fee, ogp_img, start_at, end_at, publish_at,
-            updated_at, page_view,
-        ))
-        .get_result::<EventPublic>(&conn)?)
+    Ok(
+        diesel::update(events.filter(id.eq(query_input.id.or_error("not found")?)))
+            .set(updates)
+            .returning(returns)
+            .get_result::<EventPublic>(&conn)?,
+    )
 }
 
 pub fn create(ctx: &Context, event: EventInput) -> anyhow::Result<Event> {
@@ -44,7 +61,7 @@ pub fn delete(ctx: &Context, event_id: i32) -> anyhow::Result<Event> {
     Ok(diesel::delete(events.filter(id.eq(event_id))).get_result(&conn)?)
 }
 
-pub fn list(ctx: &Context, by: Option<EventQuery>) -> anyhow::Result<Vec<Event>> {
+pub fn list(ctx: &Context, by: Option<EventListQuery>) -> anyhow::Result<Vec<Event>> {
     let conn = ctx.pool.get()?;
     let mut query = events::table.into_boxed();
     if let Some(by_event) = by {
@@ -65,7 +82,7 @@ pub fn list(ctx: &Context, by: Option<EventQuery>) -> anyhow::Result<Vec<Event>>
     Ok(query.load::<Event>(&conn)?)
 }
 
-pub fn list_public(ctx: &Context, by: Option<EventQuery>) -> anyhow::Result<Vec<EventPublic>> {
+pub fn list_public(ctx: &Context, by: Option<EventListQuery>) -> anyhow::Result<Vec<EventPublic>> {
     let conn = ctx.pool.get()?;
     let mut query = events::table.into_boxed();
     if let Some(by_event) = by {
